fe347b1eced30dd05bd1a5141655f5fc10988bf5
Yasmin-jones <90312885+Yasmin-jones@users.noreply.github.com>
2023-10-02 10:49:41 +0100

    creating drug usage template

--- app/sbna-poc/v1_0__initial-form/fields.ts
+++ app/sbna-poc/v1_0__initial-form/fields.ts
@@ -1042,6 +1042,11 @@ const fields: FormWizard.Fields = {
       { text: 'Occasionally', value: 'OCCASIONALLY', kind: 'option' },
       { text: 'Not currently using this drug', value: 'NO_CURRENT_USAGE', kind: 'option' },
     ],
+    dependent: {
+      field: 'drug_use_type',
+      value: 'HEROIN',
+      displayInline: true,
+    },
     labelClasses: mediumLabel,
   },
   daily_drug_usage_treatment: {
