df68d81ff356ee38a6865c63a3a96096d6ad6a87
Lucas Cairns <lucas.cairns@gmail.com>
2023-11-23 17:32:45 +0000

    Add education and employment fields

--- app/sbna-poc/v1_0__initial-form/fields/index.ts
+++ app/sbna-poc/v1_0__initial-form/fields/index.ts
@@ -1,10 +1,12 @@
 import accommodationFields from './accommodation'
+import employmentEducationFields from './employment-education'
 import financeFields from './finance'
 import drugFields from './drugs'
 import alcoholFields from './alcohol'
 
 export default {
   ...accommodationFields,
+  ...employmentEducationFields,
   ...financeFields,
   ...drugFields,
   ...alcoholFields,
