94d814b094e32f80f76fc07e22a3d9239dd20477
Lucas Cairns <lucas.cairns@gmail.com>
2023-11-29 12:54:44 +0000

    Add OTL page to OAStub (#222)

---
+++ app/oastub/v1_0__initial-form/controllers/oneTimeLinkController.ts
@@ -0,0 +1,35 @@
+import { Response, NextFunction } from 'express'
+import FormWizard from 'hmpo-form-wizard'
+import { randomUUID } from 'crypto'
+import BaseController from '../../../common/controllers/baseController'
+import StrengthsBasedNeedsAssessmentsApiService from '../../../../server/services/strengthsBasedNeedsService'
+
+class OneTimeLinkController extends BaseController {
+  apiService: StrengthsBasedNeedsAssessmentsApiService
+
+  constructor(options: unknown) {
+    super(options)
+
+    this.apiService = new StrengthsBasedNeedsAssessmentsApiService()
+  }
+
+  async locals(req: FormWizard.Request, res: Response, next: NextFunction) {
+    try {
+      const { link } = await this.apiService.createSession({
+        userSessionId: 'ABC1234567890',
+        userAccess: 'READ_WRITE',
+        oasysAssessmentId: randomUUID(),
+        userDisplayName: 'Probation User',
+        crn: 'X123456',
+      })
+
+      res.locals.otl = link
+
+      super.locals(req, res, next)
+    } catch (error) {
+      next(error)
+    }
+  }
+}
+
+export default OneTimeLinkController
