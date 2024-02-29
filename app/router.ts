316f757c08e656b3f51e8c0ff0b5b9a7c2b82cae
Lucas Cairns <lucas.cairns@gmail.com>
2024-02-29 16:09:47 +0000

    Improve error handling on controllers

--- app/router.ts
+++ app/router.ts
@@ -1,6 +1,12 @@
-import Express from 'express'
+import Express, { type Response, NextFunction } from 'express'
+import FormWizard from 'hmpo-form-wizard'
+import { HTTPError } from 'superagent'
 import oastubFormRouter from './oastub/index'
 import pocFormRouter from './sbna-poc/index'
+
+interface FormWizardError extends HTTPError {
+  redirect?: string
+}
 
 export default () => {
   const router = Express.Router()
@@ -11,6 +17,11 @@ export default () => {
 
   router.use('/oastub', oastubFormRouter)
   router.use('/sbna-poc', pocFormRouter)
+
+  router.use((error: FormWizardError, req: FormWizard.Request, res: Response, next: NextFunction) => {
+    if (error.redirect) return res.redirect(error.redirect)
+    return next(error)
+  })
 
   return router
 }
