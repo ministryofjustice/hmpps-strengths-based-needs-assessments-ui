e6ac0dd33d1e3e7dc7f857c261049987e92badce
Lucas Cairns <lucas.cairns@gmail.com>
2024-03-15 11:41:43 +0000

    ARN-1897 - Add initial test coverage for accommodation status fields

---
+++ cypress/e2e/v1.0/accommodation/questions/accommodationStatus.ts
@@ -0,0 +1,123 @@
+export default (stepUrl: string, summaryPage: string, positionNumber: number) => {
+  const question = "What is Paul's current accommodation?"
+
+  describe(question, () => {
+    const options = {
+      settled: 'Settled',
+      temporary: 'Temporary',
+      noAccommodation: 'No accommodation',
+    }
+
+    it('displays and validates the question', () => {
+      cy.getQuestion(question)
+        .isQuestionNumber(positionNumber)
+        // .hasHint('') TODO: Update to test for HTML hint?
+        .hasRadios([
+          options.settled,
+          options.temporary,
+          options.noAccommodation,
+        ])
+
+      cy.saveAndContinue()
+
+      cy.assertStepUrlIs(stepUrl)
+      cy.getQuestion(question).hasValidationError('Select current accommodation')
+
+    })
+
+    const typesOfSettledAccommodation = [
+      'Homeowner',
+      'Living with friends or family',
+      'Renting privately',
+      'Renting from social, local authority or other',
+      'Residential healthcare',
+      'Supported accommodation',
+    ]
+
+    it(`displays and validates the conditional options for ${options.settled}`, () => {
+      cy.getQuestion(question).getRadio(options.settled).hasConditionalQuestion(false).clickLabel()
+      cy.getQuestion(question).getRadio(options.settled).getConditionalQuestion().hasRadios(typesOfSettledAccommodation)
+
+      cy.saveAndContinue()
+
+      cy.assertStepUrlIs(stepUrl)
+      cy.getQuestion(question).getRadio(options.settled).getConditionalQuestion().hasValidationError('Select the type of settled accommodation')
+    })
+
+    typesOfSettledAccommodation.forEach(typeOfSettledAccommodation => {
+      it(`summary page displays "${options.settled} - ${typeOfSettledAccommodation}"`, () => {
+        cy.visitStep(stepUrl)
+        cy.getQuestion(question).getRadio(options.settled).clickLabel()
+        cy.getQuestion(question).getRadio(options.settled).getConditionalQuestion().getRadio(typeOfSettledAccommodation).clickLabel()
+
+        cy.saveAndContinue()
+
+        cy.visitStep(summaryPage)
+        cy.getSummary(question).getAnswer(options.settled).hasSecondaryAnswer(typeOfSettledAccommodation)
+      })
+    })
+
+    const typesOfTemporaryAccommodation = [
+      'Approved premises',
+      'Community Accommodation Service Tier 2 (CAS2)',
+      'Community Accommodation Service Tier 3 (CAS3)',
+      'Immigration accommodation',
+      'Short term accommodation'
+    ]
+
+    it(`displays and validates the conditional options for ${options.temporary}`, () => {
+      cy.getQuestion(question).getRadio(options.temporary).hasConditionalQuestion(false).clickLabel()
+      cy.getQuestion(question).getRadio(options.temporary).getConditionalQuestion().hasRadios(typesOfTemporaryAccommodation)
+
+      cy.saveAndContinue()
+
+      cy.assertStepUrlIs(stepUrl)
+      cy.getQuestion(question).getRadio(options.temporary).getConditionalQuestion().hasValidationError('Select the type of temporary accommodation')
+    })
+
+    typesOfTemporaryAccommodation.forEach(typeOfTemporaryAccommodation => {
+      it(`summary page displays "${options.temporary} - ${typeOfTemporaryAccommodation}"`, () => {
+        cy.visitStep(stepUrl)
+        cy.getQuestion(question).getRadio(options.temporary).clickLabel()
+        cy.getQuestion(question).getRadio(options.temporary).getConditionalQuestion().getRadio(typeOfTemporaryAccommodation).clickLabel()
+
+        cy.saveAndContinue()
+
+        cy.visitStep(summaryPage)
+        cy.getSummary(question).getAnswer(options.temporary).hasSecondaryAnswer(typeOfTemporaryAccommodation)
+      })
+    })
+
+    const typesOfNoAccommodation = [
+      'Awaiting assessment',
+      'Campsite',
+      'Emergency hostel',
+      'Homeless',
+      'Rough sleeping',
+      'Shelter'
+    ]
+
+    it(`displays and validates the conditional options for ${options.noAccommodation}`, () => {
+      cy.getQuestion(question).getRadio(options.noAccommodation).hasConditionalQuestion(false).clickLabel()
+      cy.getQuestion(question).getRadio(options.noAccommodation).getConditionalQuestion().hasRadios(typesOfNoAccommodation)
+
+      cy.saveAndContinue()
+
+      cy.assertStepUrlIs(stepUrl)
+      cy.getQuestion(question).getRadio(options.noAccommodation).getConditionalQuestion().hasValidationError('Select the type of no accommodation')
+    })
+
+    typesOfNoAccommodation.forEach(typeOfNoAccommodation => {
+      it(`summary page displays "${options.noAccommodation} - ${typeOfNoAccommodation}"`, () => {
+        cy.visitStep(stepUrl)
+        cy.getQuestion(question).getRadio(options.noAccommodation).clickLabel()
+        cy.getQuestion(question).getRadio(options.noAccommodation).getConditionalQuestion().getRadio(typeOfNoAccommodation).clickLabel()
+
+        cy.saveAndContinue()
+
+        cy.visitStep(summaryPage)
+        cy.getSummary(question).getAnswer(options.noAccommodation).hasSecondaryAnswer(typeOfNoAccommodation)
+      })
+    })
+  })
+}
