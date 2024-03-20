5a396e0c71b09dfbd8307ebaad992a063a15355e
devkokov <dm.kokov@gmail.com>
2024-03-20 13:20:24 +0000

    ARN-1898 Add Practitioner Analysis tests for all sections

---
+++ cypress/e2e/v1.0/alcohol-use/steps/practitioner-analysis.cy.ts
@@ -0,0 +1,3 @@
+import testPractitionerAnalysis from '../../common/practitioner-analysis/testPractitionerAnalysis'
+
+testPractitionerAnalysis('/alcohol-analysis', '/alcohol-analysis-complete')
---
+++ cypress/e2e/v1.0/thinking-behaviours-and-attitudes/steps/practitioner-analysis.cy.ts
@@ -0,0 +1,6 @@
+import testPractitionerAnalysis from '../../common/practitioner-analysis/testPractitionerAnalysis'
+
+testPractitionerAnalysis(
+  '/thinking-behaviours-attitudes-summary-analysis',
+  '/thinking-behaviours-attitudes-analysis-complete',
+)
