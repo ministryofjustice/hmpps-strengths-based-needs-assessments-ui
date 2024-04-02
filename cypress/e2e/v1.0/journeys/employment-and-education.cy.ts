7ee0aed4648514949a7daea5aba51176a062b82c
Yasmin-jones <90312885+Yasmin-jones@users.noreply.github.com>
2024-04-02 14:55:45 +0100

    Reverting back to no option in journey

--- cypress/e2e/v1.0/journeys/employment-and-education.cy.ts
+++ cypress/e2e/v1.0/journeys/employment-and-education.cy.ts
@@ -60,11 +60,7 @@ describe('Origin: /employment-education', () => {
           .getRadio('Entry level')
           .clickLabel()
 
-        cy.getQuestion('Does Sam have any professional or vocational qualifications?')
-          .getRadio('Yes')
-          .clickLabel()
-          .getConditionalQuestion()
-          .enterText('Some details')
+        cy.getQuestion('Does Sam have any professional or vocational qualifications?').getRadio('No').clickLabel()
 
         cy.getQuestion('Does Sam have skills that could help them in a job or at work?').getRadio('No').clickLabel()
 
