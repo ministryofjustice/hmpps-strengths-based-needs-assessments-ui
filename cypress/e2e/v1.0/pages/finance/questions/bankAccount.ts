ad7f43e4f25714bc1da1c0242e6088007a382023
Yasmin-jones <90312885+Yasmin-jones@users.noreply.github.com>
2024-04-04 15:55:57 +0100

    Updated EF/24 in cypress file

--- cypress/e2e/v1.0/pages/finance/questions/bankAccount.ts
+++ cypress/e2e/v1.0/pages/finance/questions/bankAccount.ts
@@ -1,17 +1,17 @@
 export default (stepUrl: string, summaryPage: string, positionNumber: number) => {
-    const question = 'Does Sam have a personal bank account?'
+    const question = 'Does Sam have their own bank account?'
     const options = ['Yes', 'No', 'Unknown']
     
     describe(question, () => {
         it(`displays and validates the question`, () => {
             cy.getQuestion(question)
               .isQuestionNumber(positionNumber)
-              .hasHint('This does not include solely having a joint account.')
+              .hasHint(null)
               .hasRadios(options)
             cy.saveAndContinue()
             cy.assertStepUrlIs(stepUrl)
             cy.getQuestion(question).hasValidationError(
-              'Select if they have a personal bank account',
+              'Select if they have their own personal bank account',
             )
             cy.checkAccessibility()
          })
