55571cf7d9155a6f58bfea572e6cbda7e8590027
Yasmin-jones <90312885+Yasmin-jones@users.noreply.github.com>
2024-04-08 15:53:21 +0100

    Adding 'Makechanges?' question to Cypress file

---
+++ cypress/e2e/v1.0/pages/finance/questions/wantToMakeChanges.ts
@@ -0,0 +1,84 @@
+export default (stepUrl: string, summaryPage: string, positionNumber: number) => {
+  const question = 'Does Sam want to make changes to their finance?'
+  const options = [
+    'I have already made positive changes and want to maintain them',
+    'I am actively making changes',
+    'I want to make changes and know how to',
+    'I want to make changes but need help',
+    'I am thinking about making changes',
+    'I do not want to make changes',
+    'I do not want to answer',
+    null,
+    'Sam is not present',
+    'Not applicable',
+  ]
+
+  describe(question, () => {
+    it(`displays and validates the question`, () => {
+      cy.getQuestion(question)
+        .isQuestionNumber(positionNumber)
+        .hasHint('This question must be directly answered by Sam.')
+        .hasRadios(options)
+      cy.saveAndContinue()
+      cy.assertStepUrlIs(stepUrl)
+      cy.getQuestion(question).hasValidationError(
+        'Select if they want to make changes to their finance',
+      )
+      cy.checkAccessibility()
+    })
+    ;[
+      'I have already made positive changes and want to maintain them',
+      'I am actively making changes',
+      'I want to make changes and know how to',
+      'I want to make changes but need help',
+      'I am thinking about making changes',
+      'I do not want to make changes',
+    ].forEach(option => {
+      it(`conditional field is displayed for "${option}"`, () => {
+        cy.getQuestion(question).getRadio(option).hasHint(null).hasConditionalQuestion(false).clickLabel()
+
+        cy.getQuestion(question)
+          .getRadio(option)
+          .getConditionalQuestion()
+          .hasTitle('Give details (optional)')
+          .hasHint(null)
+          .hasLimit(400)
+
+        cy.saveAndContinue()
+        cy.getQuestion(question)
+          .hasNoValidationError()
+          .getRadio(option)
+          .getConditionalQuestion()
+          .hasNoValidationError()
+          .enterText('some text')
+
+        cy.checkAccessibility()
+
+        cy.saveAndContinue()
+        cy.visitStep(summaryPage)
+        cy.getSummary(question).getAnswer(option).hasSecondaryAnswer('some text')
+        cy.checkAccessibility()
+        cy.getSummary(question).clickChange()
+        cy.assertStepUrlIs(stepUrl)
+        cy.assertQuestionUrl(question)
+      })
+    })
+    ;['I do not want to answer', 'Sam is not present', 'Not applicable'].forEach(option => {
+      it(`no conditional field is displayed for "${option}"`, () => {
+        cy.getQuestion(question).getRadio(option).hasHint(null).hasConditionalQuestion(false).clickLabel()
+
+        cy.getQuestion(question).getRadio(option).hasConditionalQuestion(false)
+
+        cy.saveAndContinue()
+        cy.getQuestion(question).hasNoValidationError()
+
+        cy.visitStep(summaryPage)
+        cy.getSummary(question).getAnswer(option).hasNoSecondaryAnswer()
+        cy.checkAccessibility()
+        cy.getSummary(question).clickChange()
+        cy.assertStepUrlIs(stepUrl)
+        cy.assertQuestionUrl(question)
+      })
+    })
+  })
+}
