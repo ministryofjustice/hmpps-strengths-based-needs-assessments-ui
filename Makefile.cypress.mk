177601cddd049411df0816d8273544e98f922b84
devkokov <dm.kokov@gmail.com>
2024-04-09 11:04:55 +0100

    ARN-2152 Fix TTY issue

--- Makefile.cypress.mk
+++ Makefile.cypress.mk
@@ -8,7 +8,7 @@ e2e: ## Run the end-to-end tests in the Cypress app. Override the default base U
 e2e-ci: e2e-accommodation e2e-alcohol-use e2e-drug-use e2e-employment-education e2e-finance e2e-health-wellbeing e2e-personal-relationships e2e-thinking-behaviours e2e-journeys e2e-misc
 
 e2e-accommodation e2e-alcohol-use e2e-drug-use e2e-employment-education e2e-finance e2e-health-wellbeing e2e-personal-relationships e2e-thinking-behaviours e2e-journeys e2e-misc:
-	docker compose ${TEST_COMPOSE_FILES} -p ${PROJECT_NAME}-test run --rm -e CYPRESS_BASE_URL=${BASE_URL_CI} cypress ${CYPRESS_OPTIONS}
+	docker compose ${TEST_COMPOSE_FILES} -p ${PROJECT_NAME}-test run --rm --interactive=false -e CYPRESS_BASE_URL=${BASE_URL_CI} cypress ${CYPRESS_OPTIONS}
 
 BASE_URL ?= "http://localhost:3000"
 BASE_URL_CI ?= "http://ui:3000"
@@ -24,7 +24,7 @@ E2E_PERSONAL_RELATIONSHIPS = "cypress/e2e/**/personal-relationships-and-communit
 E2E_THINKING_BEHAVIOURS = "cypress/e2e/**/thinking-behaviours-and-attitudes/**/*.cy.ts"
 E2E_JOURNEYS = "cypress/e2e/**/journeys/**/*.cy.ts"
 
-# List of all named spec patterns, so that a "catch-all" pattern can be created by excluding these patterns
+# List of all named spec patterns, so that a "catch-all" pattern can be created by excluding the named patterns
 E2E_ALL_NAMED = ${E2E_ACCOMMODATION},${E2E_ALCOHOL_USE},${E2E_DRUG_USE},${E2E_EMPLOYMENT_EDUCATION},${E2E_FINANCE},${E2E_HEALTH_WELLBEING},${E2E_PERSONAL_RELATIONSHIPS},${E2E_THINKING_BEHAVIOURS},${E2E_JOURNEYS}
 
 e2e-accommodation: CYPRESS_OPTIONS = --spec ${E2E_ACCOMMODATION}
