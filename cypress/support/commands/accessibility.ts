b735c63718e2e6bcabf155b6f7db4f736a881bc3
devkokov <dm.kokov@gmail.com>
2024-04-11 20:28:47 +0100

    ARN-2152 Fix clearBeforeRun

--- cypress/support/commands/accessibility.ts
+++ cypress/support/commands/accessibility.ts
@@ -29,7 +29,7 @@ export const checkAccessibility = (injectAxe: boolean = true) => {
 
 export const clearBeforeRun = () => {
   fs.rmSync(violationsTempPath, { force: true })
-  fs.mkdirSync(violationsTempPath, { recursive: true })
+  fs.mkdirSync('test_results/cypress', { recursive: true })
   fs.writeFileSync(violationsTempPath, '', { flag: 'w+' })
 }
 
