309ca43fe97b78a1047ac5bdb10c5519e95e9e46
Yasmin-jones <90312885+Yasmin-jones@users.noreply.github.com>
2024-04-15 09:17:50 +0100

    Arn 1899 cypress tests finance section (#412)

---
+++ cypress/e2e/v1.0/pages/finance/questions/gambling.ts
@@ -0,0 +1,60 @@
+export default (stepUrl: string, summaryPage: string, positionNumber: number) => {
+  const question = 'Is Sam affected by gambling?'
+  describe(question, () => {
+    const options = ['Yes, their own gambling', "Yes, someone else's gambling", null, 'No', 'Unknown']
+
+    it(`displays and validates the question`, () => {
+      cy.getQuestion(question).isQuestionNumber(positionNumber).hasHint('Select all that apply.').hasCheckboxes(options)
+      cy.saveAndContinue()
+      cy.assertStepUrlIs(stepUrl)
+      cy.getQuestion(question).hasValidationError('Select if they are affected by gambling')
+      cy.checkAccessibility()
+    })
+    ;['Yes, their own gambling', "Yes, someone else's gambling", 'Unknown'].forEach(option => {
+      it(`conditional field is displayed for "${option}"`, () => {
+        cy.getQuestion(question).getCheckbox(option).hasConditionalQuestion(false).clickLabel()
+
+        cy.getQuestion(question)
+          .getCheckbox(option)
+          .getConditionalQuestion()
+          .hasTitle('Give details (optional)')
+          .hasHint(null)
+          .hasLimit(400)
+
+        cy.saveAndContinue()
+        cy.getQuestion(question)
+          .hasNoValidationError()
+          .getCheckbox(option)
+          .getConditionalQuestion()
+          .hasNoValidationError()
+          .enterText('some text')
+
+        cy.checkAccessibility()
+
+        cy.saveAndContinue()
+        cy.visitStep(summaryPage)
+        cy.getSummary(question).getAnswer(option).hasSecondaryAnswer('some text')
+        cy.checkAccessibility()
+        cy.getSummary(question).clickChange()
+        cy.assertStepUrlIs(stepUrl)
+        cy.assertQuestionUrl(question)
+      })
+    })
+
+    it(`no conditional field is displayed for "No"`, () => {
+      cy.getQuestion(question).getCheckbox('No').hasConditionalQuestion(false).clickLabel()
+
+      cy.getQuestion(question).getCheckbox('No').hasConditionalQuestion(false)
+
+      cy.saveAndContinue()
+      cy.getQuestion(question).hasNoValidationError()
+
+      cy.visitStep(summaryPage)
+      cy.getSummary(question).getAnswer('No').hasNoSecondaryAnswer()
+      cy.checkAccessibility()
+      cy.getSummary(question).clickChange()
+      cy.assertStepUrlIs(stepUrl)
+      cy.assertQuestionUrl(question)
+    })
+  })
+}
