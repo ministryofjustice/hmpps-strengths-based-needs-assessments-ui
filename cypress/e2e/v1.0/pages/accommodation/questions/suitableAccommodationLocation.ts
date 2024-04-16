d531ccbfe7fce8584bdb85dc2d7ce78de4efeab1
Yasmin-jones <90312885+Yasmin-jones@users.noreply.github.com>
2024-04-16 14:47:01 +0100

    AC13 cypress tests updated

--- cypress/e2e/v1.0/pages/accommodation/questions/suitableAccommodationLocation.ts
+++ cypress/e2e/v1.0/pages/accommodation/questions/suitableAccommodationLocation.ts
@@ -32,7 +32,6 @@ export default (stepUrl: string, summaryPage: string, positionNumber: number) =>
       'Close to someone who has victimised them',
       'Close to victim or possible victims',
       'Difficulty with neighbours',
-      'Honour-based perpetrator or victim',
       'Safety of the area',
       'Other',
     ]
