65fa87e8c20a1f9bef83b6484f70bfa78d83fb1f
Dimitar Kokov <dimitar.kokov@digital.justice.gov.uk>
2024-06-13 16:18:53 +0100

    ARN-1900 Journey test for Drug use section (#487)

--- cypress/e2e/v1.0/journeys/accommodation.cy.ts
+++ cypress/e2e/v1.0/journeys/accommodation.cy.ts
@@ -14,15 +14,15 @@ describe('Origin: /accommodation', () => {
 
   const sectionName = 'Accommodation'
 
+  before(() => {
+    cy.createAssessment()
+  })
+
+  beforeEach(() => {
+    cy.enterAssessment()
+  })
+
   describe(`Destination: ${destinations.settled}`, () => {
-    before(() => {
-      cy.createAssessment()
-    })
-
-    beforeEach(() => {
-      cy.enterAssessment()
-    })
-
     const typeOfAccommodation = 'Settled'
 
     Array.of(
@@ -73,14 +73,6 @@ describe('Origin: /accommodation', () => {
   })
 
   describe(`Destination: ${destinations.temporary}`, () => {
-    before(() => {
-      cy.createAssessment()
-    })
-
-    beforeEach(() => {
-      cy.enterAssessment()
-    })
-
     const typeOfAccommodation = 'Temporary'
 
     Array.of('Immigration accommodation', 'Short term accommodation').forEach(typeOfTemporaryAccommodation => {
@@ -106,7 +98,7 @@ describe('Origin: /accommodation', () => {
       it(`routes to ${destinations.analysis}`, () => {
         cy.visitStep(destinations.temporary)
 
-        cy.getQuestion('Who is Sam living with?').getCheckbox('Alone').clickLabel()
+        cy.getQuestion('Who is Sam living with?').getCheckbox('Other').clickLabel()
 
         cy.getQuestion("Is the location of Sam's accommodation suitable?").getRadio('Yes').clickLabel()
 
@@ -126,14 +118,6 @@ describe('Origin: /accommodation', () => {
   })
 
   describe(`Destination: ${destinations.temporary2}`, () => {
-    before(() => {
-      cy.createAssessment()
-    })
-
-    beforeEach(() => {
-      cy.enterAssessment()
-    })
-
     const typeOfAccommodation = 'Temporary'
 
     Array.of(
@@ -179,14 +163,6 @@ describe('Origin: /accommodation', () => {
   })
 
   describe(`Destination: ${destinations.noAccommodation}`, () => {
-    before(() => {
-      cy.createAssessment()
-    })
-
-    beforeEach(() => {
-      cy.enterAssessment()
-    })
-
     const typeOfAccommodation = 'No accommodation'
 
     Array.of('Campsite', 'Emergency hostel', 'Homeless', 'Rough sleeping', 'Shelter').forEach(typeOfNoAccommodation => {
@@ -228,14 +204,6 @@ describe('Origin: /accommodation', () => {
   })
 
   describe(`Destination: ${destinations.noAccommodation2}`, () => {
-    before(() => {
-      cy.createAssessment()
-    })
-
-    beforeEach(() => {
-      cy.enterAssessment()
-    })
-
     const typeOfAccommodation = 'No accommodation'
 
     Array.of('Awaiting assessment').forEach(typeOfNoAccommodation => {
---
+++ cypress/e2e/v1.0/journeys/drug-use.cy.ts
@@ -0,0 +1,109 @@
+import { testPractitionerAnalysis } from './common'
+
+describe('Origin: /drug-use', () => {
+  const destinations = {
+    landingPage: '/drug-use',
+    drugUseDetails: '/drug-use-details',
+    drugTypes: '/drug-use-type',
+    drugTypesDetails: '/drug-usage-details',
+    changes: '/drug-use-changes',
+    analysis: '/drug-use-analysis',
+    analysisComplete: '/drug-use-analysis-complete',
+  }
+
+  const sectionName = 'Drug use'
+
+  before(() => {
+    cy.createAssessment()
+  })
+
+  beforeEach(() => {
+    cy.enterAssessment()
+  })
+
+  describe(`Destination: ${destinations.analysis}`, () => {
+    it(`No drug use routes to "${destinations.analysis}"`, () => {
+      cy.visitStep(destinations.landingPage)
+      cy.getQuestion('Has Sam ever used drugs?').getRadio('No').clickLabel()
+      cy.saveAndContinue()
+      cy.assertStepUrlIs(destinations.analysis)
+      cy.assertResumeUrlIs(sectionName, destinations.analysis)
+    })
+
+    testPractitionerAnalysis(sectionName, destinations.analysis, destinations.analysisComplete)
+  })
+
+  describe(`Destination: ${destinations.drugUseDetails}`, () => {
+    it(`"Drug use routes to "${destinations.drugUseDetails}"`, () => {
+      cy.visitStep(destinations.landingPage)
+      cy.getQuestion('Has Sam ever used drugs?').getRadio('Yes').clickLabel()
+      cy.saveAndContinue()
+      cy.assertStepUrlIs(destinations.drugUseDetails)
+      cy.assertResumeUrlIs(sectionName, destinations.drugUseDetails)
+    })
+
+    describe(`Destination: ${destinations.drugTypes}`, () => {
+      it(`routes to ${destinations.drugTypes}`, () => {
+        cy.visitStep(destinations.drugUseDetails)
+
+        cy.getQuestion('Why did Sam start using drugs?').getCheckbox('Enhance performance').clickLabel()
+
+        cy.getQuestion("What's the impact of Sam using drugs?").getCheckbox('Behavioural').clickLabel()
+
+        cy.getQuestion('Has anything helped Sam to stop or reduce using drugs in the past?')
+          .getRadio('Yes')
+          .clickLabel()
+
+        cy.getQuestion('Is Sam motivated to stop or reduce their drug use?')
+          .getRadio('Motivated to stop or reduce')
+          .clickLabel()
+
+        cy.saveAndContinue()
+        cy.assertStepUrlIs(destinations.drugTypes)
+        cy.assertResumeUrlIs(sectionName, destinations.drugTypes)
+      })
+
+      describe(`Destination: ${destinations.drugTypesDetails}`, () => {
+        it(`routes to ${destinations.drugTypesDetails}`, () => {
+          cy.visitStep(destinations.drugTypes)
+
+          cy.getQuestion('Which drugs has Sam used?').getCheckbox('Cannabis').clickLabel()
+
+          cy.saveAndContinue()
+          cy.assertStepUrlIs(destinations.drugTypesDetails)
+          cy.assertResumeUrlIs(sectionName, destinations.drugTypesDetails)
+        })
+
+        describe(`Destination: ${destinations.changes}`, () => {
+          it(`routes to ${destinations.changes}`, () => {
+            cy.visitStep(destinations.drugTypesDetails)
+
+            cy.getQuestion('How often is Sam using this drug?').getRadio('Daily').clickLabel()
+
+            cy.getQuestion('Has Sam used this drug in the past?').getRadio('Yes').clickLabel()
+
+            cy.saveAndContinue()
+            cy.assertStepUrlIs(destinations.changes)
+            cy.assertResumeUrlIs(sectionName, destinations.changes)
+          })
+
+          describe(`Destination: ${destinations.analysis}`, () => {
+            it(`routes to ${destinations.analysis}`, () => {
+              cy.visitStep(destinations.changes)
+
+              cy.getQuestion('Does Sam want to make changes to their drug use?')
+                .getRadio('I do not want to make changes')
+                .clickLabel()
+
+              cy.saveAndContinue()
+              cy.assertStepUrlIs(destinations.analysis)
+              cy.assertResumeUrlIs(sectionName, destinations.analysis)
+            })
+
+            testPractitionerAnalysis(sectionName, destinations.analysis, destinations.analysisComplete)
+          })
+        })
+      })
+    })
+  })
+})
