e490817849843b776aa4d16eef4646b80ff77f91
Lucas Cairns <lucas.cairns@gmail.com>
2024-06-14 14:01:50 +0100

    ARN-1902 - add page test for health and wellbeing

---
+++ cypress/e2e/v1.0/pages/health-and-wellbeing/health-wellbeing.cy.ts
@@ -0,0 +1,23 @@
+import mentalHealthCondition from './questions/mentalHealthCondition'
+import physicalHealthCondition from './questions/physicalHealthCondition'
+
+describe('/health-wellbeing', () => {
+  const stepUrl = '/health-wellbeing'
+  const summaryPage = '/health-wellbeing-analysis'
+  const questions = [physicalHealthCondition, mentalHealthCondition]
+
+  beforeEach(() => {
+    cy.createAssessment().enterAssessment()
+    cy.visitSection('Health and wellbeing')
+    cy.assertStepUrlIs(stepUrl)
+    cy.assertQuestionCount(questions.length)
+  })
+
+  questions.forEach((questionTest, index) => {
+    questionTest(stepUrl, summaryPage, index + 1)
+  })
+
+  it('Should have no accessibility violations', () => {
+    cy.checkAccessibility()
+  })
+})
