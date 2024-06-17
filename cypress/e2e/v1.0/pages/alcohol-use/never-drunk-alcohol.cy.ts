e2b0380e5d38996d67b334873df94a22d858f468
Yasmin-jones <90312885+Yasmin-jones@users.noreply.github.com>
2024-06-17 16:05:56 +0100

    Alcohol Use Cypress tests

---
+++ cypress/e2e/v1.0/pages/alcohol-use/never-drunk-alcohol.cy.ts
@@ -0,0 +1,50 @@
+import alcoholUse from './questions/alcohol-use'
+import alcoholFrequency from './questions/alcohol-frequency'
+import alcoholUnits from './questions/alcohol-units'
+// import alcohol binge drinking - add question once approved
+import evidenceOfBingeDrinking from './questions/alcohol-evidence-binge-drinking'
+import impactOfAlcohol from './questions/alcohol-impact-of-use'
+import pastAlcoholIssues from './questions/alcohol-past-issues'
+import reasonsForUse from './questions/alcohol-reasons-for-use'
+import stoppedOrReducedAlcohol from './questions/alcohol-stopped-or-reduced'
+import wantToMakeChanges from './questions/wantToMakeChanges'
+
+describe('/alcohol-use', () => {
+  const stepUrl = '/alcohol-use'
+  const summaryPage = '/alcohol-analysis'
+  const questions = [
+    alcoholUse,
+    alcoholFrequency,
+    alcoholUnits,
+    evidenceOfBingeDrinking,
+    impactOfAlcohol,
+    pastAlcoholIssues,
+    reasonsForUse,
+    stoppedOrReducedAlcohol,
+    wantToMakeChanges,
+  ]
+
+  before(() => {
+    cy.createAssessment().enterAssessment()
+    cy.visitSection('Alcohol Use')
+    cy.getQuestion("Has Sam ever drunk alcohol?").getRadio('No').clickLabel()
+    cy.saveAndContinue()
+    cy.assertStepUrlIs(stepUrl)
+    cy.captureAssessment()
+  })
+
+  beforeEach(() => {
+    cy.cloneCapturedAssessment().enterAssessment()
+    cy.visitStep(stepUrl)
+    cy.assertQuestionCount(questions.length)
+    cy.hasAutosaveEnabled()
+  })
+
+  questions.forEach((questionTest, index) => {
+    questionTest(stepUrl, summaryPage, index + 1)
+  })
+
+  it('Should have no accessibility violations', () => {
+    cy.checkAccessibility()
+  })
+})
--- cypress/e2e/v1.0/pages/alcohol-use/questions/alcohol-units.ts
+++ cypress/e2e/v1.0/pages/alcohol-use/questions/alcohol-units.ts
@@ -1,34 +1,32 @@
-// export default (stepUrl: string, summaryPage: string, positionNumber: number) => {
-//   const question = 'How many units of alcohol does Sam have on a typical day of drinking?'
-//   const options = ['1 to 2 units', '3 to 4 units', '5 to 6 units', '7 to 9 units', '10 or more units']
+export default (stepUrl: string, summaryPage: string, positionNumber: number) => {
+  const question = 'How many units of alcohol does Sam have on a typical day of drinking?'
+  const options = ['1 to 2 units', '3 to 4 units', '5 to 6 units', '7 to 9 units', '10 or more units']
 
-//   describe(question, () => {
-//     it(`displays and validates the question`, () => {
-//       cy.getQuestion(question)
-//       .isQuestionNumber(positionNumber)
-//       .hasHint('Help with alcohol units')
-//       .hasRadios(options)
-//       cy.saveAndContinue()
-//       cy.assertStepUrlIs(stepUrl)
-//       cy.getQuestion(question).hasValidationError('Select how many units of alcohol they have on a typical day of drinking')
-//       cy.checkAccessibility()
-//     })
+  describe(question, () => {
+    it(`displays and validates the question`, () => {
+      cy.getQuestion(question)
+      .isQuestionNumber(positionNumber)
+      .hasHint('Help with alcohol units')
+      .hasRadios(options)
+      cy.saveAndContinue()
+      cy.assertStepUrlIs(stepUrl)
+      cy.getQuestion(question).hasValidationError('Select how many units of alcohol they have on a typical day of drinking')
+      cy.checkAccessibility()
+    })
 
-//     options.forEach(option => {
-//       it(`summary page displays "${option}"`, () => {
-//         cy.visitStep(stepUrl)
-//         cy.getQuestion(question).getRadio(option).clickLabel()
+    options.forEach(option => {
+      it(`summary page displays "${option}"`, () => {
+        cy.visitStep(stepUrl)
+        cy.getQuestion(question).getRadio(option).clickLabel()
 
-//         cy.saveAndContinue()
+        cy.saveAndContinue()
 
-//         cy.visitStep(summaryPage)
-//         cy.getSummary(question).getAnswer(option).hasNoSecondaryAnswer()
-//         cy.getSummary(question).clickChange()
-//         cy.assertStepUrlIs(stepUrl)
-//         cy.assertQuestionUrl(question)
-//       })
-//     })
-//   })
-// }
-
-// NOTE: this question may change - awaiting for updates 
+        cy.visitStep(summaryPage)
+        cy.getSummary(question).getAnswer(option).hasNoSecondaryAnswer()
+        cy.getSummary(question).clickChange()
+        cy.assertStepUrlIs(stepUrl)
+        cy.assertQuestionUrl(question)
+      })
+    })
+  })
+}
