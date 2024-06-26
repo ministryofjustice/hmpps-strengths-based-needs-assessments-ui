36aa5b4ca7a3789ba72a55c1da1ff04633f1e596
Yasmin-jones <90312885+Yasmin-jones@users.noreply.github.com>
2024-06-26 15:12:20 +0100

    Adding TBA questions to Cypress folder

---
+++ cypress/e2e/v1.0/pages/thinking-behaviours-and-attitudes/questions/problem-solving.ts
@@ -0,0 +1,29 @@
+export default (stepUrl: string, summaryPage: string, positionNumber: number) => {
+  const question = 'Is Sam able to solve problems in a positive way?'
+  const options = ['Yes, is able to solve problems and identify appropriate solutions', 'Has limited problem solving skills', 'No, has poor problem solving skills and is unable to identify what steps to take to solve a problem']
+
+  describe(question, () => {
+    it(`displays and validates the question`, () => {
+      cy.getQuestion(question).isQuestionNumber(positionNumber).hasRadios(options)
+      cy.saveAndContinue()
+      cy.assertStepUrlIs(stepUrl)
+      cy.getQuestion(question).hasValidationError('Select if they are able to solve problems in a positive way')
+      cy.checkAccessibility()
+    })
+
+    options.forEach(option => {
+      it(`summary page displays "${option}"`, () => {
+        cy.visitStep(stepUrl)
+        cy.getQuestion(question).getRadio(option).clickLabel()
+
+        cy.saveAndContinue()
+
+        cy.visitStep(summaryPage)
+        cy.getSummary(question).getAnswer(option).hasNoSecondaryAnswer()
+        cy.getSummary(question).clickChange()
+        cy.assertStepUrlIs(stepUrl)
+        cy.assertQuestionUrl(question)
+      })
+    })
+  })
+}
---
+++ cypress/e2e/v1.0/pages/thinking-behaviours-and-attitudes/questions/stable-behaviour.ts
@@ -0,0 +1,29 @@
+export default (stepUrl: string, summaryPage: string, positionNumber: number) => {
+  const question = 'Does Sam show stable behaviour?'
+  const options = ['Yes, shows stable behaviour', 'Sometimes shows stable behaviour but can show reckless or risk taking behaviours', 'No, shows reckless or risk taking behaviours']
+
+  describe(question, () => {
+    it(`displays and validates the question`, () => {
+      cy.getQuestion(question).isQuestionNumber(positionNumber).hasRadios(options)
+      cy.saveAndContinue()
+      cy.assertStepUrlIs(stepUrl)
+      cy.getQuestion(question).hasValidationError('Select if they show stable behaviour')
+      cy.checkAccessibility()
+    })
+
+    options.forEach(option => {
+      it(`summary page displays "${option}"`, () => {
+        cy.visitStep(stepUrl)
+        cy.getQuestion(question).getRadio(option).clickLabel()
+
+        cy.saveAndContinue()
+
+        cy.visitStep(summaryPage)
+        cy.getSummary(question).getAnswer(option).hasNoSecondaryAnswer()
+        cy.getSummary(question).clickChange()
+        cy.assertStepUrlIs(stepUrl)
+        cy.assertQuestionUrl(question)
+      })
+    })
+  })
+}
