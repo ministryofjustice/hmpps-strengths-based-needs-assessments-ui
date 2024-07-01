6b33608bf6ef5dff196a88eeabde7e0ecec81db9
Yasmin-jones <90312885+Yasmin-jones@users.noreply.github.com>
2024-07-01 10:37:22 +0100

    Adding accessibility check to question tests

--- cypress/e2e/v1.0/pages/thinking-behaviours-and-attitudes/questions/peoples-views.ts
+++ cypress/e2e/v1.0/pages/thinking-behaviours-and-attitudes/questions/peoples-views.ts
@@ -21,10 +21,13 @@ export default (stepUrl: string, summaryPage: string, positionNumber: number) =>
         cy.visitStep(stepUrl)
         cy.getQuestion(question).getRadio(option).clickLabel()
 
+        cy.checkAccessibility()
+
         cy.saveAndContinue()
 
         cy.visitStep(summaryPage)
         cy.getSummary(question).getAnswer(option).hasNoSecondaryAnswer()
+        cy.checkAccessibility()
         cy.getSummary(question).clickChange()
         cy.assertStepUrlIs(stepUrl)
         cy.assertQuestionUrl(question)
