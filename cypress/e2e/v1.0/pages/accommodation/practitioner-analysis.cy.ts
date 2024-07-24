5da117ae0b63c0f710e2653438728b0e9129d128
Lucas Cairns <lucas.cairns@gmail.com>
2024-07-24 14:37:45 +0100

    Update PA page tests

--- cypress/e2e/v1.0/pages/accommodation/practitioner-analysis.cy.ts
+++ cypress/e2e/v1.0/pages/accommodation/practitioner-analysis.cy.ts
@@ -13,8 +13,14 @@ before(() => {
     .getRadio('Homeowner')
     .clickLabel()
   cy.saveAndContinue()
-  cy.assertResumeUrlIs('Accommodation', summaryPage)
 
+  cy.getQuestion('Who is Sam living with?').getCheckbox('Alone').clickLabel()
+  cy.getQuestion("Is the location of Sam's accommodation suitable?").getRadio('Yes').clickLabel()
+  cy.getQuestion("Is Sam's accommodation suitable?").getRadio('Yes').clickLabel()
+  cy.getQuestion('Does Sam want to make changes to their accommodation?').getRadio('Not applicable').clickLabel()
+  cy.saveAndContinue()
+
+  cy.assertResumeUrlIs('Accommodation', summaryPage)
   cy.captureAssessment()
 })
 
--- cypress/e2e/v1.0/pages/finance/practitioner-analysis.cy.ts
+++ cypress/e2e/v1.0/pages/finance/practitioner-analysis.cy.ts
@@ -7,7 +7,15 @@ before(() => {
 
   cy.visitSection('Finance')
   cy.getQuestion('Where does Sam currently get their money from?').getCheckbox('No money').clickLabel()
+  cy.getQuestion('Does Sam have their own bank account?').getRadio('Unknown').clickLabel()
+  cy.getQuestion('How good is Sam at managing their money?')
+    .getRadio('Able to manage their money well and is a strength')
+    .clickLabel()
+  cy.getQuestion('Is Sam affected by gambling?').getRadio('Unknown').clickLabel()
+  cy.getQuestion('Is Sam affected by debt?').getRadio('Unknown').clickLabel()
+  cy.getQuestion('Does Sam want to make changes to their finance?').getRadio('Not applicable').clickLabel()
   cy.saveAndContinue()
+
   cy.assertStepUrlIs(summaryPage)
   cy.assertResumeUrlIs('Finance', summaryPage)
 
