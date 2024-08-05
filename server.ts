b7f2540eaf721b3d1495859d8087ffb165458139
devkokov <dm.kokov@gmail.com>
2024-08-05 16:10:34 +0100

    ARN-2297 Set keep-alive to 30s for e2e tests

--- server.ts
+++ server.ts
@@ -4,15 +4,9 @@ import 'applicationinsights'
 import { app, metricsApp } from './server/index'
 import logger from './logger'
 
-const server = app.listen(app.get('port'), () => {
+app.listen(app.get('port'), () => {
   logger.info(`Server listening on port ${app.get('port')}`)
-})
-
-server.keepAliveTimeout = 30000
-
-logger.info(`Keep alive timeout: ${server.keepAliveTimeout}`)
-logger.info(`Max connections: ${server.maxConnections}`)
-logger.info(`Max reqs per socket: ${server.maxRequestsPerSocket}`)
+}).keepAliveTimeout = Number.parseInt(process.env.KEEP_ALIVE || "5000")
 
 metricsApp.listen(metricsApp.get('port'), () => {
   logger.info(`Metrics server listening on port ${metricsApp.get('port')}`)
