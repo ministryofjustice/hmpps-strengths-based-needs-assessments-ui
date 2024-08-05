5ad4eb3ecf3157173633fd092e85579e0cd41ab7
devkokov <dm.kokov@gmail.com>
2024-08-05 14:37:18 +0100

    ARN-2297 Increase keepAlive timeout

--- server.ts
+++ server.ts
@@ -4,9 +4,15 @@ import 'applicationinsights'
 import { app, metricsApp } from './server/index'
 import logger from './logger'
 
-app.listen(app.get('port'), () => {
+const server = app.listen(app.get('port'), () => {
   logger.info(`Server listening on port ${app.get('port')}`)
 })
+
+server.keepAliveTimeout = 30000
+
+logger.info(`Keep alive timeout: ${server.keepAliveTimeout}`)
+logger.info(`Max connections: ${server.maxConnections}`)
+logger.info(`Max reqs per socket: ${server.maxRequestsPerSocket}`)
 
 metricsApp.listen(metricsApp.get('port'), () => {
   logger.info(`Metrics server listening on port ${metricsApp.get('port')}`)
