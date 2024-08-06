77730d9f925d74030806fa8a9235605fecd3046a
devkokov <dm.kokov@gmail.com>
2024-08-06 10:15:58 +0100

    Fix linting

--- app/controllers/startController.ts
+++ app/controllers/startController.ts
@@ -32,7 +32,7 @@ const startController = async (req: Request, res: Response, next: NextFunction) 
         ? res.redirect(`/form/${version}/${readOnlyModeLandingPage}`)
         : res.redirect(`/form/${version}/${editModeLandingPage}?action=resume`)
     })
-  } catch (error) {
+  } catch (_) {
     next(new Error('Unable to start assessment'))
   }
 }
