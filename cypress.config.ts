263805cccbd0cb2ef16e50cac64d45e1db6d16f8
devkokov <dm.kokov@gmail.com>
2024-08-06 14:16:25 +0100

    Fix linting

--- cypress.config.ts
+++ cypress.config.ts
@@ -28,7 +28,6 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       on('task', {
         table(message) {
-          // eslint-disable-next-line no-console
           console.table(message)
           return null
         },
