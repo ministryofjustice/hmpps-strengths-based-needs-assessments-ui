ceb9b43a2269724162df9daa9eb5a487ae778f37
devkokov <dm.kokov@gmail.com>
2024-09-30 11:31:29 +0100

    ARN-2310 Fix linting

--- server/errorHandler.test.ts
+++ server/errorHandler.test.ts
@@ -1,12 +1,5 @@
-import type { Express } from 'express'
 import request from 'supertest'
 import { appWithAllRoutes } from './routes/testutils/appSetup'
-
-let app: Express
-
-beforeEach(() => {
-  app = appWithAllRoutes({})
-})
 
 afterEach(() => {
   jest.resetAllMocks()
