2ffe0ac69b0a67e43e1f950688caf0ffcafa7c93
devkokov <dm.kokov@gmail.com>
2024-10-14 13:17:33 +0100

    Add typecheck to build

--- esbuild/app.config.js
+++ esbuild/app.config.js
@@ -2,9 +2,10 @@ const { copy } = require('esbuild-plugin-copy')
 const esbuild = require('esbuild')
 const glob = require('glob')
 const pkg = require('../package.json')
+const { spawn } = require("child_process");
 
-const buildApp = buildConfig =>
-  esbuild.build({
+const buildApp = buildConfig => {
+  const build = () => esbuild.build({
     entryPoints: glob.sync(buildConfig.app.entryPoints),
     outdir: buildConfig.app.outDir,
     bundle: true,
@@ -19,6 +20,15 @@ const buildApp = buildConfig =>
       }),
     ],
   })
+
+  return new Promise((resolve, reject) => {
+    const typecheck = spawn('npx tsc --noEmit', { stdio: 'pipe', shell: true })
+    const log = (data) => { console.log(data.toString()) }
+    typecheck.stdout.on('data', log)
+    typecheck.stderr.on('data', log)
+    typecheck.on('exit', (code) => code !== 0 ? reject(`Type check failed.`) : build().then(resolve))
+  })
+}
 
 module.exports = buildConfig => {
   console.log('\u{1b}[1m\u{2728}  Building app...\u{1b}[0m')
