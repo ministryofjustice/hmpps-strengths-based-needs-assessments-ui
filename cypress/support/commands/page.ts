0494ee974c21e8c50b267d04d362ff2ed53a008d
devkokov <dm.kokov@gmail.com>
2024-10-28 12:59:38 +0000

    Fix e2e assertions

--- cypress/support/commands/page.ts
+++ cypress/support/commands/page.ts
@@ -34,11 +34,11 @@ export const currentSectionNotMarkedAsComplete = (section: string) => {
 }
 
 export const assessmentMarkedAsComplete = () => {
-  cy.get('.form-header__actions > button').should('not.have.class', 'govuk-button--secondary')
+  cy.get('.form-header__actions > .govuk-button').should('not.have.class', 'govuk-button--secondary')
 }
 
 export const assessmentNotMarkedAsComplete = () => {
-  cy.get('.form-header__actions > button').should('have.class', 'govuk-button--secondary')
+  cy.get('.form-header__actions > .govuk-button').should('have.class', 'govuk-button--secondary')
 }
 
 export const hasAutosaveEnabled = () => {
