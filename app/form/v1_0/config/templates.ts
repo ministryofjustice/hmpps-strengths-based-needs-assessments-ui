41ca967e22542170c09c0e84b6fa501fb341fdb9
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2025-04-22 15:04:49 +0100

    Add new templates for practitioner-analysis

--- app/form/v1_0/config/templates.ts
+++ app/form/v1_0/config/templates.ts
@@ -3,4 +3,6 @@ export default {
   analysisIncomplete: 'forms/summary/summary-analysis-incomplete',
   drugUsage: 'forms/drug-usage',
   printPreview: 'pages/print-preview',
+  practitionerAnalysisComplete: 'forms/practitioner-analysis-complete',
+  practitionerAnalysisIncomplete: 'forms/practitioner-analysis-incomplete',
 }
