45f5befba087e7fd07e4c5966670ed3379683a4d
Phil Wilson <github@philwilson.org>
2025-05-01 15:42:37 +0100

    Updates first field to have correct options.

--- app/form/v1_0/fields/drug-use/drug-use-history.ts
+++ app/form/v1_0/fields/drug-use/drug-use-history.ts
@@ -6,28 +6,28 @@ import { dependentOn } from '../common/utils'
 
 class DrugUseHistoryFieldsFactory extends FieldsFactory {
   drugsReasonsForUse: FormWizard.Field = {
-    text: 'Why does [subject] drink alcohol?',
-    hint: { text: 'Select all that apply.', kind: 'text' },
-    code: 'alcohol_reasons_for_use',
+    text: 'Why does [subject] use drugs?',
+    hint: { text: 'Consider why they started using, their history, any triggers.', kind: 'text' },
+    code: 'drugs_reasons_for_use',
     type: FieldType.CheckBox,
     multiple: true,
     validate: [{ type: ValidationType.Required, message: 'Select why they drink alcohol' }],
     options: [
       { text: 'Cultural or religious practice', value: 'CULTURAL_OR_RELIGIOUS', kind: 'option' },
       { text: 'Curiosity or experimentation', value: 'CURIOSITY_OR_EXPERIMENTATION', kind: 'option' },
-      { text: 'Enjoyment', value: 'ENJOYMENT', kind: 'option' },
+      { text: 'Enhance performance', value: 'ENHANCE_PERFORMANCE', kind: 'option' },
+      { text: 'Escapism or avoidance', value: 'ESCAPISM_OR_AVOIDANCE', kind: 'option' },
       { text: 'Manage stress or emotional issues', value: 'MANAGING_EMOTIONAL_ISSUES', kind: 'option' },
-      { text: 'On special occasions', value: 'SPECIAL_OCCASIONS', kind: 'option' },
       { text: 'Peer pressure or social influence', value: 'PEER_PRESSURE', kind: 'option' },
-      {
-        text: 'Self-medication or mood altering',
-        hint: { text: 'Includes pain management or emotional regulation.' },
-        value: 'SELF_MEDICATION',
-        kind: 'option',
-      },
-      { text: 'Socially', value: 'SOCIAL', kind: 'option' },
+      { text: 'Recreation or pleasure', value: 'RECREATION_OR_PLEASURE', kind: 'option' },
+      { text: 'Self-medication', value: 'SELF_MEDICATION', kind: 'option' },
       { text: 'Other', value: 'OTHER', kind: 'option' },
     ],
     labelClasses: utils.getMediumLabelClassFor(FieldType.CheckBox),
   }
+
+  drugsReasonsForUseDetails: FormWizard.Field = FieldsFactory.detailsField({
+    parentField: this.drugsReasonsForUse,
+    dependentValue: 'YES',
+  })
 }
