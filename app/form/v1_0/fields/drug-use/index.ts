e24596527e1a621e3672ff41a27061056d682f8b
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2025-05-06 10:01:50 +0100

    Removed unused fields groups and updated imports

--- app/form/v1_0/fields/drug-use/index.ts
+++ app/form/v1_0/fields/drug-use/index.ts
@@ -6,24 +6,18 @@ import addDrugs from './add-drugs'
 // import drugDetails from './drug-details'
 import drugUse from './drug-use'
 import drugUseAnalysis from './drug-use-analysis'
-// import drugUseAnalysisSummary from './drug-use-analysis-summary'
 import drugUseHistory from './drug-use-history'
-// import drugUseSummary from './drug-use-summary'
 
 class DrugUse extends FieldsFactory {
-  readonly addDrugs: Record<string, FormWizard.Field>
+  readonly addDrugs
 
-  readonly drugDetails: Record<string, FormWizard.Field>
+  // readonly drugDetails
 
-  readonly drugUse: Record<string, FormWizard.Field>
+  readonly drugUse
 
-  readonly drugUseAnalysis: Record<string, FormWizard.Field>
+  readonly drugUseAnalysis
 
-  readonly drugUseAnalysisSummary: Record<string, FormWizard.Field>
-
-  readonly drugUseHistory: Record<string, FormWizard.Field>
-
-  readonly drugUseSummary: Record<string, FormWizard.Field>
+  readonly drugUseHistory
 
   constructor(section: Section) {
     super(section)
@@ -32,9 +26,7 @@ class DrugUse extends FieldsFactory {
     // this.drugDetails = drugDetails
     this.drugUse = drugUse
     this.drugUseAnalysis = drugUseAnalysis
-    // this.drugUseAnalysisSummary = drugUseAnalysisSummary
     this.drugUseHistory = drugUseHistory
-    // this.drugUseSummary = drugUseSummary
   }
 }
 
