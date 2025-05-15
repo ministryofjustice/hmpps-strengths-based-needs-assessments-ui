080a97b9a7d2ac33a5d55eb590fe4fc34349e93d
Phil Wilson <github@philwilson.org>
2025-05-15 16:53:44 +0100

    Fixes failing navigation tests

--- cypress/support/commands/navigation.ts
+++ cypress/support/commands/navigation.ts
@@ -5,7 +5,11 @@ export const visitSection = (name: string) => {
 
 export const assertSectionIs = (name: string) => {
   cy.get(`.side-navigation li.moj-side-navigation__item--active`).should('have.length', 1).and('contain.text', name)
-  cy.get(`h2`).should('contain.text', name)
+
+  // for some sections the name is in the caption, in others the h2
+  cy.get('.section-heading__heading')
+    .contains(new RegExp(`^${name}$`))
+    .should('be.visible')
 }
 
 export const visitStep = (path: string) => {
