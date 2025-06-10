b8a6870f178e9d6973be81c310c07e8f487d052b
Iana-Bezrodnykh <iana.bezrodnykh@justice.gov.uk>
2025-06-10 18:28:54 +0100

    ACE-149: Add controller and fetch service

---
+++ app/controllers/viewVersionListController.ts
@@ -0,0 +1,16 @@
+import { Response, NextFunction } from 'express'
+import FormWizard from 'hmpo-form-wizard'
+import SaveAndContinueController from './saveAndContinueController'
+import { SessionData } from '../../server/services/strengthsBasedNeedsService'
+
+export default class ViewVersionListController extends SaveAndContinueController {
+  async locals(req: FormWizard.Request, res: Response, next: NextFunction) {
+    try {
+      const sessionData = req.session.sessionData as SessionData
+      res.locals.assessmentVersions = this.apiService.fetchAssessmentVersions(sessionData.assessmentId)
+      await super.locals(req, res, next)
+    } catch (error) {
+      next(error)
+    }
+  }
+}
