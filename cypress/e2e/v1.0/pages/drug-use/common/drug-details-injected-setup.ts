b5c7c894e6e82f5ae7abba4c7faa045a5c2ab9dc
Phil Wilson <pipwilson@users.noreply.github.com>
2025-06-12 15:07:08 +0100

    Makes e2e tests run faster (#841)

---
+++ cypress/e2e/v1.0/pages/drug-use/common/drug-details-injected-setup.ts
@@ -0,0 +1,54 @@
+import { Fixture } from '../../../../../support/commands/fixture'
+import { drugName, drugs } from './drugs'
+import whichDrugsInjected from '../questions/whichDrugsInjected'
+import receivingTreatment from '../questions/receivingTreatment'
+
+export const stepUrl = '/drug-details-injected'
+export const summaryPage = '/drug-use-summary'
+export const questions = [whichDrugsInjected(true), receivingTreatment(true)]
+
+export const drugDetailsInjectedBefore = () => () => {
+  cy.loadFixture(Fixture.DrugUser).enterAssessment()
+  cy.visitSection('Drug use')
+
+  drugs.forEach(({ name: drug }) => {
+    cy.getQuestion('Which drugs has Sam misused?').getCheckbox(drug).clickLabel()
+    if (drug === 'Other') {
+      cy.getQuestion('Which drugs has Sam misused?').getCheckbox(drug).getNthConditionalQuestion(0).enterText('Cake')
+      cy.getQuestion('Which drugs has Sam misused?')
+        .getCheckbox(drug)
+        .getNthConditionalQuestion(1)
+        .getRadio('Used in the last 6 months')
+        .clickLabel()
+    } else {
+      cy.getQuestion('Which drugs has Sam misused?')
+        .getCheckbox(drug)
+        .getConditionalQuestion()
+        .getRadio('Used in the last 6 months')
+        .clickLabel()
+    }
+  })
+  cy.saveAndContinue()
+
+  cy.assertStepUrlIs(stepUrl)
+  cy.hasDrugQuestionGroups(drugs.length)
+  drugs.forEach(({ name: drug }) => {
+    cy.hasQuestionsForDrug(drugName(drug), 2)
+  })
+  cy.assertQuestionCount(questions.length)
+
+  cy.hasAutosaveEnabled()
+  cy.hasFeedbackLink()
+  cy.assertResumeUrlIs('Drug use', stepUrl)
+
+  cy.hasSubheading('Not used in the last 6 months', false)
+  cy.hasSubheading('Used in the last 6 months', true)
+
+  cy.captureAssessment()
+}
+
+export const drugDetailsInjectedBeforeEach = () => () => {
+  cy.cloneCapturedAssessment().enterAssessment()
+  cy.visitSection('Drug use')
+  cy.assertStepUrlIs(stepUrl)
+}
