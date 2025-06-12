f701f94b6e546b2c85621a5abbc6088e79f52c80
JakeGoodwin22 <103187669+JakeGoodwin22@users.noreply.github.com>
2025-06-12 14:41:24 +0100

    ACE-113: Data Privacy Screen (#843)

---
+++ app/controllers/dataPrivacyController.ts
@@ -0,0 +1,43 @@
+import { NextFunction, Response } from 'express'
+import FormWizard from 'hmpo-form-wizard'
+import BaseController from './baseController'
+import { SessionData } from '../../server/services/strengthsBasedNeedsService'
+import privacyScreenFields from '../form/v1_0/fields/privacy-screen'
+import { withPlaceholdersFrom } from '../utils/field.utils'
+import { HandoverSubject } from '../../server/services/arnsHandoverService'
+import config from '../../server/config'
+
+class DataPrivacyController extends BaseController {
+  constructor(options: unknown) {
+    super(options)
+  }
+
+  async configure(req: FormWizard.Request, res: Response, next: NextFunction) {
+    try {
+      const sessionData = req.session.sessionData as SessionData
+      const subjectDetails = req.session.subjectDetails as HandoverSubject
+      const placeholderValues = { subject: subjectDetails.givenName }
+
+      res.locals.user = {
+        ...(res.locals.user || {}),
+        ...(sessionData?.user || {}),
+        username: sessionData?.user?.displayName || '',
+      }
+
+      res.locals.subjectDetails = subjectDetails
+      res.locals.sessionData = sessionData
+      res.locals.placeholderValues = placeholderValues
+      res.locals.oasysUrl = config.oasysUrl
+      res.locals.isPrivacyScreen = true
+
+      const field = privacyScreenFields.privacyScreenDeclaration()
+      res.locals.privacyField = withPlaceholdersFrom(placeholderValues)(field)
+
+      return await super.configure(req, res, next)
+    } catch (error) {
+      return next(error)
+    }
+  }
+}
+
+export default DataPrivacyController
