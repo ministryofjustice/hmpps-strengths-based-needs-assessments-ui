1efa96cab7f81d2447f237264af6f8edac22c737
devkokov <dm.kokov@gmail.com>
2025-07-01 16:41:12 +0100

    ACE-149 Add CoordinatorService to viewVersionListController.ts

--- app/controllers/viewVersionListController.ts
+++ app/controllers/viewVersionListController.ts
@@ -2,8 +2,17 @@ import { Response, NextFunction } from 'express'
 import FormWizard from 'hmpo-form-wizard'
 import SaveAndContinueController from './saveAndContinueController'
 import { SessionData } from '../../server/services/strengthsBasedNeedsService'
+import CoordinatorApiService from '../../server/services/CoordinatorService'
 
 export default class ViewVersionListController extends SaveAndContinueController {
+  protected coordinatorService: CoordinatorApiService
+
+  constructor(options: unknown) {
+    super(options)
+
+    this.coordinatorService = new CoordinatorApiService()
+  }
+
   async locals(req: FormWizard.Request, res: Response, next: NextFunction) {
     try {
       const sessionData = req.session.sessionData as SessionData
