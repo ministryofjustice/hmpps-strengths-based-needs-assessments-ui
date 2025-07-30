59c0e69a9686b53e142dd30d87fcfa99da1cdbc3
izzyL-DJ <isaiah.lakanu@digital.justice.gov.uk>
2025-07-30 10:33:10 +0100

    update e2e

--- cypress/e2e/v1.0/pages/offence-analysis/questions/elementsOfTheOffence.ts
+++ cypress/e2e/v1.0/pages/offence-analysis/questions/elementsOfTheOffence.ts
@@ -60,6 +60,71 @@ export default (stepUrl: string, summaryPage: string, positionNumber: number) =>
       })
     })
 
+    // Array.of('Weapon').forEach(option => {
+    //   it(`conditional field is displayed for "${option}"`, () => {
+    //     cy.getQuestion(question).getCheckbox(option).hasConditionalQuestion(false).clickLabel()
+    //
+    //     cy.getQuestion(question)
+    //       .getCheckbox(option)
+    //       .getNthConditionalQuestion(0)
+    //       .hasHint('')
+    //       .hasNoValidationError()
+    //       .hasText('')
+    //     cy.getQuestion(question)
+    //       .getCheckbox(option)
+    //       .getNthConditionalQuestion(1)
+    //       .hasHint(null)
+    //       .hasNoValidationError()
+    //     cy.saveAndContinue()
+    //
+    //     cy.assertStepUrlIs(stepUrl)
+    //     cy.getQuestion(question)
+    //       .getCheckbox(option)
+    //       .getNthConditionalQuestion(0)
+    //       .hasValidationError(`Enter which other option they've misused`)
+    //     cy.getQuestion(question)
+    //       .getCheckbox(option)
+    //       .getNthConditionalQuestion(1)
+    //       .hasValidationError(`Select when they last used this drug`)
+    //
+    //     cy.checkAccessibility()
+    //   })
+    // })
+
+    Array.of('Weapon').forEach(option => {
+      it(`conditional field is displayed for "${option}"`, () => {
+        cy.getQuestion(question).getCheckbox(option).hasConditionalQuestion(false).clickLabel()
+
+        cy.getQuestion(question)
+          .getCheckbox(option)
+          .getConditionalQuestion()
+          .hasTitle('What was the weapon?')
+          .enterText('This text will be over 200 characters long.This text will be over 200 characters long.This text will be over 200 characters long.This text will be over 200 characters long.This text will be over 200 characters long.')
+          .hasHint(null)
+
+        cy.saveAndContinue()
+
+        cy.getQuestion(question)
+          .hasNoValidationError()
+          .getCheckbox(option)
+          .getConditionalQuestion()
+          .hasValidationError('Weapon must be 200 characters or les')
+
+        cy.getQuestion(question).getCheckbox(option).getConditionalQuestion().enterText('some text')
+
+        cy.checkAccessibility()
+
+        cy.saveAndContinue()
+        cy.visitStep(summaryPage)
+        cy.getSummary(question).getAnswer(option).hasSecondaryAnswer('some text')
+        cy.checkAccessibility()
+        cy.getSummary(question).clickChange()
+        cy.assertStepUrlIs(stepUrl)
+        cy.assertQuestionUrl(question)
+        cy.getQuestion(question).getCheckbox(option).isChecked().getConditionalQuestion().hasText('some text')
+      })
+    })
+
     Array.of(
       'Arson',
       'Domestic abuse',
@@ -68,7 +133,6 @@ export default (stepUrl: string, summaryPage: string, positionNumber: number) =>
       'Physical damage to property',
       'Sexual element',
       'Violence, or threat of violence or coercion',
-      'Weapon',
       'None',
     ).forEach(option => {
       it(`no conditional field is displayed for "${option}"`, () => {
