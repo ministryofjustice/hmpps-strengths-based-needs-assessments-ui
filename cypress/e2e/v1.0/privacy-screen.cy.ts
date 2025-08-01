1c2061d02f125bef53f8afc8f8bfc64edf2a1841
JakeGoodwin22 <103187669+JakeGoodwin22@users.noreply.github.com>
2025-08-01 14:39:42 +0100

    ACE-391: Update privacy screen content

--- cypress/e2e/v1.0/privacy-screen.cy.ts
+++ cypress/e2e/v1.0/privacy-screen.cy.ts
@@ -9,11 +9,12 @@ describe('Data privacy declaration', () => {
   it('renders the page correctly', () => {
     cy.enterAssessment(AccessMode.READ_WRITE, {}, false)
     cy.get('.govuk-heading-l').contains(
-      'Remember to close anything you do not need before starting an appointment with Sam',
+      'Remember to close any other applications before starting an appointment with Sam',
     )
     cy.get('.govuk-body').contains('For example, Outlook, Teams or NDelius.')
     cy.get('.govuk-body').contains('You must do this to avoid sharing sensitive information.')
-    cy.get('.govuk-checkboxes').contains("I confirm I'll close anything I do not need before starting an appointment")
+    cy.get('.govuk-body').contains('You should not let Sam use your device either.')
+    cy.get('.govuk-checkboxes').contains("I confirm I'll close any other applications before starting an appointment")
     cy.get('button[name="action"][value="confirm"]').contains('Confirm')
     cy.contains('a.govuk-link--no-visited-state', 'Return to OASys')
       .should('have.attr', 'href')
@@ -23,14 +24,14 @@ describe('Data privacy declaration', () => {
   it('displays validation error when submitting form without checkbox=checked', () => {
     cy.enterAssessment(AccessMode.READ_WRITE, {}, false)
     cy.get('button[name="action"][value="confirm"]').click()
-    cy.url().should('include', '/close-anything-not-needed-before-appointment')
+    cy.url().should('include', '/close-any-other-applications-before-appointment')
     cy.get('.govuk-error-summary').should(
       'contain',
-      "Confirm you'll close anything you do not need before starting an appointment",
+      "Confirm you'll close any other applications before starting an appointment",
     )
     cy.get('#privacy_screen_declaration-error').should(
       'contain',
-      "Confirm you'll close anything you do not need before starting an appointment",
+      "Confirm you'll close any other applications before starting an appointment",
     )
   })
 
