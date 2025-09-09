59f104b84ef79bb453af4da6620f988555eafd68
“OliverYeoman” <Ollie.Yeoman@justice.gov.uk>
2025-09-09 10:40:58 +0100

    ACT-161 finance prison pathway

--- app/form/v1_0/steps/finance.ts
+++ app/form/v1_0/steps/finance.ts
@@ -1,11 +1,13 @@
-import { setFieldToIncomplete, setFieldToCompleteWhenValid } from './common'
+import { setFieldToIncomplete, setFieldToCompleteWhenValid, nextWhen } from './common'
 import financeFields from '../fields/finance'
 import sections, { SectionConfig } from '../config/sections'
 import templates from '../config/templates'
 
 const section = sections.finance
 const stepUrls = {
+  startFinance: 'start-finance',
   finance: 'finance',
+  financePrison: 'finance-prison',
   summary: 'finance-summary',
   analysis: 'finance-analysis',
 }
@@ -27,9 +29,39 @@ const baseFinanceGroup = [
   financeFields.unknownDebtDetails,
 ].flat()
 
+const baseFinancePrisonGroup = [
+  financeFields.financeIncome,
+  financeFields.financeIncomeDetails,
+  financeFields.familyOrFriendsDetails,
+  financeFields.financeIncomeInCustody,
+  financeFields.financeIncomeInCustodyDetails,
+  financeFields.financeBankAccount,
+  financeFields.financeMoneyManagement,
+  financeFields.financeMoneyManagementDetailsGroup,
+  financeFields.financeGambling,
+  financeFields.financeGamblingDetailsGroup,
+  financeFields.financeDebt,
+  financeFields.yesTypeOfDebt,
+  financeFields.yesTypeOfDebtDetailsGroup,
+  financeFields.yesSomeoneElsesTypeOfDebt,
+  financeFields.yesSomeoneElsesTypeOfDebtDetailsGroup,
+  financeFields.unknownDebtDetails,
+].flat()
+
 const sectionConfig: SectionConfig = {
   section,
   steps: [
+    {
+      url: stepUrls.startFinance,
+      next: [
+        nextWhen(financeFields.pathway, 'COMMUNITY', stepUrls.finance),
+        nextWhen(financeFields.pathway, 'PRISON', stepUrls.financePrison),
+        stepUrls.finance,
+      ],
+      sectionProgressRules: [setFieldToIncomplete(section.sectionCompleteField)],
+      navigationOrder: 3,
+      skip: true,
+    },
     {
       url: stepUrls.finance,
       fields: [
@@ -38,7 +70,17 @@ const sectionConfig: SectionConfig = {
         financeFields.isUserSubmitted(stepUrls.finance),
         financeFields.sectionComplete(),
       ].flat(),
-      navigationOrder: 3,
+      next: stepUrls.summary,
+      sectionProgressRules: [setFieldToIncomplete(section.sectionCompleteField)],
+    },
+    {
+      url: stepUrls.financePrison,
+      fields: [
+        baseFinancePrisonGroup,
+        financeFields.wantToMakeChanges(),
+        financeFields.isUserSubmitted(stepUrls.financePrison),
+        financeFields.sectionComplete(),
+      ].flat(),
       next: stepUrls.summary,
       sectionProgressRules: [setFieldToIncomplete(section.sectionCompleteField)],
     },
