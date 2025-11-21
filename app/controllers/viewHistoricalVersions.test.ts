8f091d55920e2f34e4979600cffb3dd4786376d0
JakeGoodwin22 <103187669+JakeGoodwin22@users.noreply.github.com>
2025-11-21 12:08:46 +0000

    SP2-1697: Redirect to correct previous version when coming from SP (#903)
    Co-authored-by: Phil Wilson <pipwilson@users.noreply.github.com>

---
+++ app/controllers/viewHistoricalVersions.test.ts
@@ -0,0 +1,58 @@
+import { NextFunction, Request, Response } from 'express'
+import StrengthsBasedNeedsAssessmentsApiService, {
+  AssessmentResponse,
+} from '../../server/services/strengthsBasedNeedsService'
+import ArnsHandoverService from '../../server/services/arnsHandoverService'
+import viewHistoricalVersions from './viewHistoricalVersions'
+
+jest.mock('../../server/services/strengthsBasedNeedsService')
+
+jest.mock('../../server/services/arnsHandoverService')
+
+describe('viewHistoricalVersions', () => {
+  const session = {
+    save: jest.fn(),
+  }
+
+  const req = {
+    session,
+    params: {},
+  } as unknown as Request
+
+  const res = {
+    locals: { user: { token: 'mockToken' } },
+    redirect: jest.fn(),
+  } as unknown as Response
+
+  const next = jest.fn() as NextFunction
+
+  const assessmentUUID = crypto.randomUUID()
+  const assessmentVersionUUID = crypto.randomUUID()
+  const fetchAssessment = StrengthsBasedNeedsAssessmentsApiService.prototype.fetchAssessment as jest.Mock
+
+  beforeEach(() => {
+    ;(ArnsHandoverService.prototype.getContextData as jest.Mock).mockReset()
+    fetchAssessment.mockReset().mockResolvedValue({
+      assessment: {},
+      metaData: { formVersion: '1.0', uuid: assessmentUUID, versionUuid: assessmentVersionUUID },
+    } as AssessmentResponse)
+
+    res.redirect = jest.fn()
+    session.save = jest.fn(cb => cb())
+  })
+
+  it('redirects to the view historic accommodation tasks page', async () => {
+    ;(ArnsHandoverService.prototype.getContextData as jest.Mock).mockResolvedValue({
+      assessmentContext: { assessmentId: assessmentUUID },
+      principal: { accessMode: 'READ_WRITE' },
+      handoverSessionId: 'mockSessionId',
+      subject: {},
+    })
+
+    await viewHistoricalVersions(req, res, next)
+
+    expect(session.save).toHaveBeenCalled()
+    expect(res.redirect).toHaveBeenCalledWith(`/form/view-historic/${assessmentVersionUUID}/accommodation-tasks`)
+    expect(fetchAssessment).toHaveBeenCalledWith(assessmentUUID, undefined)
+  })
+})
