82e1bdbb2c73c50796cfe2f000b895a8306d610f
Ben Howe <ben.howe1@justice.gov.uk>
2025-12-19 12:13:25 +0000

    ARN-3056: fix prettier formatting

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -2,13 +2,13 @@
 import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-allowlist'
 
 export default configureAllowedScripts({
-   allowlist: {
-      "node_modules/@parcel/watcher@2.5.1": "ALLOW",
-      "node_modules/applicationinsights-native-metrics@0.0.11": "ALLOW",
-      "node_modules/cypress@14.3.3": "ALLOW",
-      "node_modules/dtrace-provider@0.8.8": "ALLOW",
-      "node_modules/esbuild@0.25.4": "ALLOW",
-      "node_modules/fsevents@2.3.3": "ALLOW",
-      "node_modules/unrs-resolver@1.7.2": "ALLOW",
-   },
+  allowlist: {
+    'node_modules/@parcel/watcher@2.5.1': 'ALLOW',
+    'node_modules/applicationinsights-native-metrics@0.0.11': 'ALLOW',
+    'node_modules/cypress@14.3.3': 'ALLOW',
+    'node_modules/dtrace-provider@0.8.8': 'ALLOW',
+    'node_modules/esbuild@0.25.4': 'ALLOW',
+    'node_modules/fsevents@2.3.3': 'ALLOW',
+    'node_modules/unrs-resolver@1.7.2': 'ALLOW',
+  },
 })
