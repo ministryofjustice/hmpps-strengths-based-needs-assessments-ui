c4c7c114bbba4e0e327994bed823041768e858b6
Lucas Cairns <lucas.cairns@gmail.com>
2026-01-20 12:54:30 +0000

    Ministryofjustice eslint config hmpps 1.x fixed (#947)
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -1,4 +1,3 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-allowlist'
 
 export default configureAllowedScripts({
